b15b5048aef6ad92ce9a9e2b24309adb89fd323e
Luis Enrique <fernandez.luis@pucp.pe>
2024-10-16 17:43:28 -0500

    home

--- routes/ecommerce.js
+++ routes/ecommerce.js
@@ -8,13 +8,11 @@ const router = express.Router();
 const usuarioController= require('../controllers/usuario')
 const productosController= require('../controllers/productos')
 
-
 router.get('/', usuarioController.isLoggedIn, productosController.getProductosBienvenido);
 router.get('/mobile', usuarioController.isLoggedIn, productosController.getProductosMobile);
 router.get('/tv-audio', usuarioController.isLoggedIn, productosController.getProductosTvaudio);
 router.get('/electrodomesticos', usuarioController.isLoggedIn, productosController.getProductosElectrodomesticos);
 router.get('/tecnologia-ai', usuarioController.isLoggedIn, productosController.getProductosTecnologiaai);
 router.get('/ventas-especiales', usuarioController.isLoggedIn, productosController.getProductosVentasespeciales);
-
 
 module.exports = router;
